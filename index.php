8f0817b6987a16a5c30c5f715d3dffc3483058bb
HighQualitySoup <noobofguys9001@gmail.com>
2022-01-27 12:39:28 -0800

    Figure 1 HTML
    HTML for Figure 1 Wireframe, needs to add links to other pages, next step is CSS and MySQL Database

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Parking App</title>
+</head>
+<center>
+
+    <body>
+
+        <h1>Parking App</h1>
+
+        <br>
+        <!-- Email and Password text and input boxes tables -->
+        <table>
+            <tr>
+                <td>Email</td>
+            </tr>
+            <tr>
+                <td><input type="text" name="email"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+            </tr>
+            <tr>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td><a href="Password Forget Page">Forgot Password?</a> </td>
+            </tr>
+        </table>
+
+        <br>
+        <!-- Login Button -->
+        <form action="Map Page">
+            <input type="submit" value="Login" />
+        </form>
+
+        <br>
+        <!-- Don't Have an account text -->
+        <table>
+            <tr>
+                <td>Don't Have an Account?</a></td>
+            </tr>
+        </table>
+        
+        <!-- Sign Up Button -->
+        <form action="Page where users will create accounts">
+            <input type="submit" value="Sign Up" />
+        </form>
+
+    </body>
+</center>
+
+</html>
