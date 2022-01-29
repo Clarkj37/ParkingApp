fb726705c5682ef233b2606fef6239f6a671b85a
HighQualitySoup <noobofguys9001@gmail.com>
2022-01-28 16:20:44 -0800

    CSS
    Kill me this took a while

--- accountcreation.php
+++ accountcreation.php
@@ -3,61 +3,57 @@
 
 <head>
     <title>Account Creation</title>
+    <link rel="stylesheet" type="text/css" href="app.css">
 </head>
 <center>
 
     <body>
-
+        <br><br><br>
         <h1>Create Account</h1>
-
-        <br>
         <!-- Email and Password text and input boxes tables -->
-        <table>
-            <tr>
-                <td>Name</td>
-            </tr>
-            <tr>
-                <td><input type="text" name="name"></td>
-            </tr>
-            <tr>
-                <td>Email</td>
-            </tr>
-            <tr>
-                <td><input type="text" name="email"></td>
-            </tr>
-            <tr>
-                <td>Phone</td>
-            </tr>
-            <tr>
-                <td><input type="text" name="phone"></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-            </tr>
-            <tr>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <td>Confirm Password</td>
-            </tr>
-            <tr>
-                <td><input type="password" name="password"></td>
-            </tr>
-        </table>
-        <br>
-        <br>
-        <!-- Don't Have an account text -->
-        <table>
-            <tr>
-                <td>Don't Have an Account?</a></td>
-            </tr>
-        </table>
-        <br>
-        <!-- Sign Up Button -->
-        <form action="index.php">
-            <input type="submit" value="Sign Up" />
-        </form>
-
+        <div>
+            <table>
+                <br>
+                <tr>
+                    <td class="logintext">Name</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name" class = "textinput"></td>
+                </tr>
+                <tr>
+                    <td class="logintext">Email</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="email" class = "textinput"></td>
+                </tr>
+                <tr>
+                    <td class="logintext">Phone</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="phone" class = "textinput"></td>
+                </tr>
+                <tr>
+                    <td class="logintext">Password</td>
+                </tr>
+                <tr>
+                    <td><input type="password" name="password" class = "textinput"></td>
+                </tr>
+                <tr>
+                    <td class="logintext">Confirm Password</td>
+                </tr>
+                <tr>
+                    <td><input type="password" name="password" class = "textinput"></td>
+                </tr>
+            </table>
+            <br>
+            <!-- Don't Have an account text -->
+            <br>
+            <!-- Sign Up Button -->
+            <form action="index.php">
+                <input type="submit" value="Sign Up" class = "buttoninput"/>
+            </form>
+            <br>
+        </div>
     </body>
 </center>
 
--- index.php
+++ index.php
@@ -2,54 +2,54 @@
 <html>
 
 <head>
+    <link rel="stylesheet" type="text/css" href="app.css">
     <title>Parking App</title>
 </head>
 <center>
 
     <body>
-
+<br><br><br><br><br>
         <h1>Parking App</h1>
 
-        <br>
         <!-- Email and Password text and input boxes tables -->
-        <table>
-            <tr>
-                <td>Email</td>
-            </tr>
-            <tr>
-                <td><input type="text" name="email"></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-            </tr>
-            <tr>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <td><a href="Password Forget Page">Forgot Password?</a> </td>
-            </tr>
-        </table>
+        <div>
+            <br>
+            <table>
+                <tr>
+                    <td class = "logintext">Email</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="email" class = "textinput"></td>
+                </tr>
+                <tr class = "logintext">
+                    <td>Password</td>
+                </tr>
+                <tr>
+                    <td><input type="password" name="password" class = "textinput"></td>
+                </tr>
+                <tr>
+                    <td class = "forgotpassword"><a href="Password Forget Page">Forgot Password?</a> </td>
+                </tr>
+            </table>
 
-        <br>
-        <!-- Login Button -->
-        <form action="Map Page">
-            <input type="submit" value="Login" />
-        </form>
-
-        <br>
-        <br>
-        <!-- Don't Have an account text -->
-        <table>
-            <tr>
-                <td>Don't Have an Account?</a></td>
-            </tr>
-        </table>
-        <br>
-        <!-- Sign Up Button -->
-        <form action="accountcreation.php">
-            <input type="submit" value="Sign Up" />
-        </form>
-
+            <br>
+            <!-- Login Button -->
+            <form action="Map Page">
+                <input type="submit" value="Login" class = "buttoninput"/>
+            </form>
+            <br>
+            <!-- Don't Have an account text -->
+            <table>
+                <tr>
+                    <td class = "logintext">Don't Have an Account?</a></td>
+                </tr>
+            </table>
+            <!-- Sign Up Button -->
+            <form action="accountcreation.php">
+                <input type="submit" value="Sign Up" class = "buttoninput"/>
+            </form>
+            <br>
+        </div>
     </body>
 </center>
 
